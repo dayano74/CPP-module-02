5a61b2aa03ed06e660c98338fa8b534d560ccec9
dayano74 <kiixxn00@gmail.com>
2025-09-07 20:08:04 +0900

    add Fixed class implementation with constructors, assignment operator, and member functions

---
+++ ex00/srcs/Fixed.cpp
@@ -0,0 +1,42 @@
+#include "Fixed.hpp"
+
+#include <iostream>
+
+Fixed::Fixed()
+    : _raw(0)
+{
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+    : _raw(0)
+{
+  std::cout << "Copy constructor called" << std::endl;
+  *this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs)
+{
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &rhs) {
+    this->_raw = rhs._raw;
+  }
+  return *this;
+}
+
+Fixed::~Fixed()
+{
+  std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const
+{
+  std::cout << "getRawBits member function called" << std::endl;
+  return this->_raw;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+  std::cout << "setRawBits member function called" << std::endl;
+  this->_raw = raw;
+}
