b8ce73530adef30053f96e5af90475513d558d50
dayano74 <kiixxn00@gmail.com>
2025-09-07 20:23:56 +0900

    add commented-out code for setRawBits

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -12,5 +12,9 @@ int main( void ) {
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
+
+    // c.setRawBits(3);
+    // std::cout << c.getRawBits() << std::endl;
+
     return 0;
 }
