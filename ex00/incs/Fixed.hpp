b702721a1b96b61dbb98d5c1437eb41b5a450aab
dayano74 <kiixxn00@gmail.com>
2025-10-25 21:16:20 +0900

    refactor: refact format

--- ex00/incs/Fixed.hpp
+++ ex00/incs/Fixed.hpp
@@ -1,20 +1,19 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
-class Fixed
-{
-private:
+class Fixed {
+ private:
   int _raw;
   static const int _fractionalBits = 8;
 
-public:
+ public:
   Fixed();
   Fixed(const Fixed& other);
   Fixed& operator=(const Fixed&);
   ~Fixed();
 
   int getRawBits() const;
-  void setRawBits( int const raw );
+  void setRawBits(int const raw);
 };
 
 #endif  // FIXED_HPP
