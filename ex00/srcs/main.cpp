de6df6862df59e8e1942a097917baa0c9d99967a
dayano74 <kiixxn00@gmail.com>
2025-12-16 19:05:45 +0900

    refactor:add test cases

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cassert>
 
 #include "../incs/Fixed.hpp"
 
 int main(void) {
+  {
+  // Test case from ex00 subject
   Fixed a;
   Fixed b(a);
   Fixed c;
@@ -12,9 +15,38 @@ int main(void) {
   std::cout << a.getRawBits() << std::endl;
   std::cout << b.getRawBits() << std::endl;
   std::cout << c.getRawBits() << std::endl;
+  }
 
-  // c.setRawBits(3);
-  // std::cout << c.getRawBits() << std::endl;
+  {
+    Fixed a;
+    assert(a.getRawBits() == 0);
+  }
 
+  {
+    Fixed a;
+    a.setRawBits(123);
+
+    Fixed b(a);
+    Fixed c;
+    c = a;
+
+    assert(a.getRawBits() == 123);
+    assert(b.getRawBits() == 123);
+    assert(c.getRawBits() == 123);
+
+    b.setRawBits(42);
+    assert(a.getRawBits() == 123);
+    assert(b.getRawBits() == 42);
+    assert(c.getRawBits() == 123);
+  }
+
+  {
+    Fixed a;
+    a.setRawBits(123);
+    a = a;
+    assert(a.getRawBits() == 123);
+  }
+
+  std::cout << "Tests passed.\n";
   return 0;
 }
